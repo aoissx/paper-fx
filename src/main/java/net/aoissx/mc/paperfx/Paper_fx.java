53fe3879a7cf1a70db432cd643b2270096216c71
aoissx <113966503+aoissx@users.noreply.github.com>
2023-10-17 03:19:44 +0900

    a

---
+++ src/main/java/net/aoissx/mc/paperfx/Paper_fx.java
@@ -0,0 +1,17 @@
+package net.aoissx.mc.paperfx;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Paper_fx extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
