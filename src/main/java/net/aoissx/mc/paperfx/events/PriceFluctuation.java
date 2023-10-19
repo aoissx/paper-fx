a962e6fa3694e0527172bf75ed957758b266f2d2
aoissx <113966503+aoissx@users.noreply.github.com>
2023-10-20 03:25:42 +0900

    確率変更

--- src/main/java/net/aoissx/mc/paperfx/events/PriceFluctuation.java
+++ src/main/java/net/aoissx/mc/paperfx/events/PriceFluctuation.java
@@ -13,6 +13,7 @@ public class PriceFluctuation implements Runnable {
     private int min;
     private int max;
     private int range;
+    private int median;
 
     public PriceFluctuation(int min, int max, int range) {
         this.priceDao = new PriceDao();
@@ -20,6 +21,7 @@ public class PriceFluctuation implements Runnable {
         this.max = max;
         this.min = min;
         this.range = range;
+        this.median = (max + min) / 2;
     }
 
     @Override
@@ -27,7 +29,7 @@ public class PriceFluctuation implements Runnable {
         int currentPrice = priceDao.getPrice();
 
         // 現在の値から-rangeから+rangeの範囲内でランダムに変動させる
-        int newPrice = currentPrice + random.nextInt(range * 2) - range;
+        int newPrice = decidePrice(currentPrice);
 
         // minとmaxの範囲内に収まるようにする
         if (newPrice < min) {
@@ -41,5 +43,31 @@ public class PriceFluctuation implements Runnable {
         Bukkit.broadcastMessage("§a§l[PaperFX] §r§a価格が変動しました。");
         // currentPriceとnewPriceを通知する
         Bukkit.broadcastMessage("§a§l[PaperFX] §r§a" + currentPrice + " -> " + newPrice);
+    }
+
+    /*
+     * 価格の変動値を設定する。
+     */
+    private int decidePrice(int currentPrice) {
+        int diff = random.nextInt(range+1) - range/2; // 変動値
+        int randomValue = random.nextInt(100); // 0~99の乱数
+        int width = (max-min)/4; // ボーダー
+        // 中央値から離れているほど確率を低くする
+        if(currentPrice < median-width && diff < 0){
+            if(randomValue < 50){
+                diff += random.nextInt(range+1)-range/2;
+            }
+            if(randomValue < 90){
+                diff = -diff;
+            }
+        }else if(currentPrice > median+width && diff > 0){
+            if(randomValue < 50){
+                diff += random.nextInt(range+1)-range/2;
+            }
+            if(randomValue < 90){
+                diff = -diff;
+            }
+        }
+        return currentPrice + diff;
     }
 }
